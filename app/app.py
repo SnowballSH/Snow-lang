15f0403f156fa0901bf97aeb28d6bf114007b7c5
SnowballSH <derek070211@gmail.com>
2020-09-21 22:22:15 -0500

    threading app

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from core.compiler.compiler import run as run_f
+
+import threading
+
+
+class AppThread(threading.Thread):
+    def __init__(self, filename):
+        threading.Thread.__init__(self)
+        self.filename = filename
+
+    def run(self):
+        run_f(self.filename)
+
+
+def run(filename, stdout="snow.stdout", stderr="snow.stderr"):
+    old_out = ""
+    old_err = ""
+    app = AppThread(filename)
+    app.start()
+    while app.is_alive():
+        with open(stdout, "r") as out:
+            f_out = out.read()
+            if f_out != old_out:
+                print(f_out)
+                old_out = f_out
+        with open(stderr, "r") as err:
+            f_err = err.read()
+            if f_err != old_err:
+                print(f_err)
+                old_err = f_err
--- test/test.py
+++ test/test.py
@@ -1,4 +1,7 @@
 from core.compiler.compiler import run
+from app import app
 from timeit import timeit
 
-print(timeit(lambda: run("test.snow"), number=10))
+# print(timeit(lambda: run("test.snow"), number=10))
+
+app.run("test.snow")
