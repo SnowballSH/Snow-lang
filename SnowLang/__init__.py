7d85426703c7e363126dbdd477d7e363e7e12bb6
SnowballSH <derek070211@gmail.com>
2020-09-29 18:00:44 -0500

    publish

--- SnowLang/__init__.py
+++ SnowLang/__init__.py
@@ -4,4 +4,4 @@ from .compiler import compiler
 __title__ = 'SnowLang'
 __author__ = 'SnowballSH'
 __license__ = 'MIT'
-__version__ = '0.5.0'
+__version__ = '0.5.1.1'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name="SnowLang",
       install_requires=requirements,
       python_requires=">=3.6",
       url="https://github.com/SnowballSH/Snow-lang",
-      download_url="https://github.com/SnowballSH/Snow-lang/archive/v0.5.0.tar.gz",
+      download_url="https://github.com/SnowballSH/Snow-lang/archive/v0.5.1.1.tar.gz",
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
